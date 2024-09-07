708785e0ca33612d8c05cef2318e2dfc064d1951
Gean-Bacinello <142836768+Gean-Bacinello@users.noreply.github.com>
2024-09-07 20:50:29 -0300

    atualizado
    mudanças no index; styles; app.js

--- app.js
+++ app.js
@@ -1,37 +1,25 @@
 function pesquisar() {
-
-    
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos.
-    // O elemento com o ID "resultados-pesquisa" será utilizado para
-    // inserir os resultados dinamicamente.
-    let section = document.getElementById
-    ("resultados-pesquisa");
+    let section = document.getElementById("resultados-pesquisa");
  
-    let  campoPesquisa = document.getElementById
-    ("campo-pesquisa").value.trim();
-    console.log(campoPesquisa)
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.trim().toLowerCase();
+    console.log(campoPesquisa);
 
-
-    if (campoPesquisa == ""){
-        section.innerHTML = " <p> nada foi encontrado digite o nome de um jogo ou genero de jogo </p>";
-        return
+    if (campoPesquisa === "") {
+        section.innerHTML = "<p>Nada foi encontrado. Digite o nome de um jogo ou gênero de jogo.</p>";
+        return;
     }
 
     // Inicializa uma string vazia para armazenar o HTML dos resultados.
-    // A cada iteração do loop, o HTML de cada resultado será concatenado
-    // a essa string, formando o HTML final que será inserido na seção.
     let resultados = "";
-    let titulo = "";
-    let genero = "";
-    // Itera sobre cada item de dados (assumindo que 'dados' é um array).
-    // Para cada item, cria uma nova div com as informações do item
-    // (título, descrição, gênero e link) e adiciona ao HTML dos resultados.
-    for (let dado of dados) {
-        titulo = dado.titulo.toLowerCase()
-        genero = dado.genero.toLowerCase()
-        if (titulo.includes(campoPesquisa)
-        || genero.includes(campoPesquisa)){
 
+    // Itera sobre cada item de dados (assumindo que 'dados' é um array).
+    for (let dado of dados) {
+        // Converte 'dado.titulo' e 'dado.genero' para minúsculas antes de usar 'includes'.
+        let tituloLower = dado.titulo.toLowerCase();
+        let generoLower = dado.genero.toLowerCase();
+
+        if (tituloLower.includes(campoPesquisa) || generoLower.includes(campoPesquisa)) {
             resultados += `
             <div class="item-resultado">
                 <h2><a href="#" target="_blank">${dado.titulo}</a></h2>  
@@ -40,14 +28,11 @@ function pesquisar() {
                 <a href="${dado.link}" target="_blank">Mais informações</a>
             </div>`;
         }
-    }    
-   
+    }
 
     // Atualiza o conteúdo HTML da seção com os resultados construídos.
-    // Substitui todo o conteúdo anterior da seção pelos novos resultados.
-    if (!resultados) {
+    if (resultados === "") {
         resultados = "<p>Nenhum resultado encontrado.</p>";
-    } else {
-        section.innerHTML = resultados;
     }
+    section.innerHTML = resultados;
 }
